5b6afe38f1a91f1dcb9c92998416cc948506a5c9
Thomas Hepner <hepner.thomas@gmail.com>
2022-12-22 10:55:02 -0800

    fix: remove Zapper alert.

--- src/core/store/modules/app/app.actions.ts
+++ src/core/store/modules/app/app.actions.ts
@@ -85,10 +85,12 @@ const checkExternalServicesStatus = createAsyncThunk<void, void, ThunkAPI>(
         'service is currently experiencing technical issues and have been temporarily disabled. We apologize for any inconvenience this may cause, we are actively working on resolving these issues';
       const downgradedServicesMessages = [];
       const { zapper, simulations } = data;
-      if (!zapper) {
-        dispatch(disableService({ service: 'zapper' }));
-        downgradedServicesMessages.push(`Zapper ${errorMessageTemplate}`);
-      }
+
+      // TODO: Reenable Zapper alerts if that is a desired feature.
+      // if (!zapper) {
+      //   dispatch(disableService({ service: 'zapper' }));
+      //   downgradedServicesMessages.push(`Zapper ${errorMessageTemplate}`);
+      // }
 
       if (!simulations) {
         dispatch(disableService({ service: 'tenderly' }));

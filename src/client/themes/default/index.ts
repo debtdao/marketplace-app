faf737910696e1d19698d1ffece7b2297c878564
Thomas Hepner <hepner.thomas@gmail.com>
2023-01-20 14:24:01 -0800

    fix: increase modal width.

--- src/client/themes/default/index.ts
+++ src/client/themes/default/index.ts
@@ -111,7 +111,7 @@ const sharedTheme: SharedTheme = {
   },
 
   txModal: {
-    width: '47rem', // original default: 38.4rem
+    width: '48rem', // original default: 38.4rem
     gap: '.8rem',
   },
 
